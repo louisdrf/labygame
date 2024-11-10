e6151b0385231c2ea7b7669a81f65acad527cca2
goodfoodtruck <mielatomique@protonmail.com>
2024-11-10 17:30:21 +0100

    added "serde" crate and serialized "Welcome" message + added a few logs

--- labygame-server/src/main.rs
+++ labygame-server/src/main.rs
@@ -1,7 +1,11 @@
-use std::{
-    io::{prelude::*, BufReader},
-    net::{TcpListener, TcpStream},
-};
+use std::io::{prelude::*, BufReader};
+use std::net::{TcpListener, TcpStream};
+use serde::Serialize;
+
+#[derive(Serialize)]
+enum Response {
+    Welcome {version: u8}
+}
 
 fn handle_connection(mut stream: TcpStream) {
     let mut buf_reader = BufReader::new(&mut stream);
@@ -9,9 +13,12 @@ fn handle_connection(mut stream: TcpStream) {
 
     match buf_reader.read_line(&mut message) {
         Ok(_) => {
+            println!("Message received: {:?}", message);
             if message.trim() == "Hello" {
-                let response = r#"{"Welcome":{"version":1}}"#;
-                stream.write_all(response.as_bytes()).unwrap();
+                let response = Response::Welcome { version: 1 };
+                let serialized = serde_json::to_string(&response).unwrap();
+                stream.write_all(serialized.as_bytes()).unwrap();
+                println!("Sent 'Welcome' message");
             }
         }
         Err(_) => {}
