183d54336a5fced2b3444aa49d354209ef207340
goodfoodtruck <mielatomique@protonmail.com>
2024-11-12 22:03:59 +0100

    better handling for response deserialization in client subscribe function

--- labygame-client/src/main.rs
+++ labygame-client/src/main.rs
@@ -1,18 +1,31 @@
 use std::io::{Write, Read};
 use std::net::TcpStream;
-use common::Response;
+use common::{Response, SubscribeError};
 
 fn subscribe(stream: &mut TcpStream) {
+    // Request
     let request = Response::Subscribe {name: String::from("Player1")};
     let serialized = serde_json::to_vec(&request).unwrap();
     stream.write_all(&serialized).unwrap();
 
+    // Response
     let mut buffer = vec![0; 128];
     let bytes_read = stream.read(&mut buffer).unwrap();
     buffer.truncate(bytes_read);
 
-    let response: Response = serde_json::from_slice(&buffer).unwrap();
-    println!("Server response: {:?}", response);
+    match serde_json::from_slice(&buffer) {
+        Ok(Response::SubscribeResult(Ok(()))) => {
+            println!("Success Subscribe !");
+        },
+        Ok(Response::SubscribeResult(Err(SubscribeError::InvalidName))) => {
+            println!("Invalid name !")
+        },
+        Ok(Response::SubscribeResult(Err(SubscribeError::AlreadyRegistered))) => {
+            println!("Name already registered !")
+        },
+        Err(_) => println!("Error while reading the subscribe response"),
+        _ => println!("Wrong answer")
+    };
 }
 
 fn main() {
