de1debf36bf51ee0ac11cc2d6913325b35816f33
Louis Driffort <louis.driffort@gmail.com>
2024-12-01 19:37:47 +0100

    split horizontal and vertical walls parsing in 2 functions, rename some variables

--- worker/src/radar_view_utils.rs
+++ worker/src/radar_view_utils.rs
@@ -10,38 +10,26 @@ fn base64_char_to_decimal(c: char) -> Result<u8, String> {
 }
 
 pub fn decode(encoded_radar_view: &str) -> i32 {
-    let mut binary: String = String::new(); 
+    let mut binary_encoded_radar_view: String = String::new(); 
 
-    for c in encoded_radar_view.chars() {
-        let c_as_base64 = base64_char_to_decimal(c).unwrap();
-        let binary_letter = format!("{:06b}", c_as_base64);
-        binary.push_str(&binary_letter);
+    for encoded_char in encoded_radar_view.chars() {
+        let encoded_char_as_base64 = base64_char_to_decimal(encoded_char).unwrap();
+        let six_bits_binary_letter = format!("{:06b}", encoded_char_as_base64);
+        binary_encoded_radar_view.push_str(&six_bits_binary_letter);
     }
 
-    // HORIZONTAL WALLS -> 24 first bits by 6 bits paquets
-    let mut horizontal_walls_reversed: String = String::new(); 
-    for i in 0..4 {
-        let part = &binary[i*6..(i+1) * 6];
-        horizontal_walls_reversed.insert_str(0, &part);
-    }
-
+    let horizontal_walls_reversed = parse_horizontal_walls_part(&binary_encoded_radar_view);
     println!("horizontal walls : {} - 4 * 6 bits", horizontal_walls_reversed);
 
 
-    // VERTICAL WALLS -> 24 next bits by 8 bits paquets
-    let mut vertical_walls_reversed: String = String::new(); 
-    for i in 3..6 {
-        let part = &binary[i*8..(i+1) * 8];
-        vertical_walls_reversed.insert_str(0, &part);
-    }
-
-    println!("horizontal walls : {} - 3 * 8 bits", vertical_walls_reversed);
+    let vertical_walls_reversed = parse_vertical_walls_part(&binary_encoded_radar_view);
+    println!("vertical walls : {} - 3 * 8 bits", vertical_walls_reversed);
 
 
     // CELLS -> last 40 bits by 8 bits paquets
     let mut cells: String = String::new(); 
     for i in 6..11 {
-        let part = &binary[i*8..(i+1) * 8];
+        let part = &binary_encoded_radar_view[i*8..(i+1) * 8];
         cells.push_str(part);
     }
 
@@ -66,6 +54,35 @@ pub fn decode(encoded_radar_view: &str) -> i32 {
 
     horizontal_walls_reversed.len() as i32
 }
+
+
+/**
+ * return the firsts 4 paquets of 6 bits of the binary string
+ */
+fn parse_horizontal_walls_part(binary_string: &str) -> String {
+    // HORIZONTAL WALLS -> 24 first bits by 6 bits paquets
+    let mut horizontal_walls_reversed: String = String::new(); 
+    for i in 0..4 {
+        let part = &binary_string[i*6..(i+1) * 6];
+        horizontal_walls_reversed.insert_str(0, &part);
+    }
+
+    horizontal_walls_reversed
+}
+
+/**
+ * return the string containing the 3 bytes representing the vertical walls from the binary string
+ */
+fn parse_vertical_walls_part(binary_string: &str) -> String {
+    let mut vertical_walls_reversed: String = String::new(); 
+    for i in 3..6 {
+        let part = &binary_string[i*8..(i+1) * 8];
+        vertical_walls_reversed.insert_str(0, &part);
+    }
+
+    return vertical_walls_reversed
+}
+
 
 
 #[cfg(test)]
