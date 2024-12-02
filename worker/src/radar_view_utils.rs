aee91770daff7e761bc39bf6b9bc1bff21a1c6d0
Louis Driffort <louis.driffort@gmail.com>
2024-12-03 00:37:08 +0100

    use RadarCell enum values to fill radar_view Vec, fix radar_view fill order with cells

--- worker/src/radar_view_utils.rs
+++ worker/src/radar_view_utils.rs
@@ -1,3 +1,22 @@
+#[derive(Clone, Debug)]
+enum RadarCell {
+        Undefined,                   // 00 
+        Open,                        // 01
+        Wall,                        // 10
+        Unknown(String)              // unknown bits combination
+}
+
+impl RadarCell {
+    pub fn from_bits(bits: &str) -> Self {
+        match bits {
+            "00" | "1111" => RadarCell::Undefined,
+            "01" | "0000" => RadarCell::Open,
+            "10"   => RadarCell::Wall,
+            _ => RadarCell::Unknown(bits.to_string())
+        }
+    }
+}
+
 pub fn decode(encoded_radar_view: &str) -> i32 {
     let mut binary_encoded_radar_view: String = String::new(); 
 
@@ -18,7 +37,7 @@ pub fn decode(encoded_radar_view: &str) -> i32 {
     let cells_part = binary_encoded_radar_view.drain(..).collect::<String>();
     let cells = parse_cells_part(&cells_part);
 
-    let radar_view: Vec<Vec<String>> = vec![vec![String::new(); 7]; 7];
+    let radar_view: Vec<Vec<RadarCell>> = vec![vec![RadarCell::Undefined; 7]; 7];
     let radar_view = fill_radar_view_with_horizontal_walls(&radar_view, &horizontal_walls_cells);
     let radar_view = fill_radar_view_with_vertical_walls(&radar_view, &vertical_walls_cells);
     let radar_view = fill_radar_view_with_cells(&radar_view, &cells);
@@ -41,42 +60,47 @@ fn base64_char_to_base10_char(c: char) -> Result<u8, String> {
     }
 }
 
-fn fill_radar_view_with_horizontal_walls(radar_view: &Vec<Vec<String>>, horizontal_walls_cells: &Vec<String>) -> Vec<Vec<String>> {
+fn fill_radar_view_with_horizontal_walls(radar_view: &Vec<Vec<RadarCell>>, horizontal_walls_cells: &Vec<String>) -> Vec<Vec<RadarCell>> {
     let mut radar_view = radar_view.clone();
     let mut offset = 0;
 
     for i in (0..radar_view.len()).step_by(2) {
-        radar_view[i][1] = horizontal_walls_cells[offset].clone();
-        radar_view[i][3] = horizontal_walls_cells[offset + 1].clone();
-        radar_view[i][5] = horizontal_walls_cells[offset + 2].clone();
+        radar_view[i][1] = RadarCell::from_bits(horizontal_walls_cells[offset].as_str());
+        radar_view[i][3] = RadarCell::from_bits(horizontal_walls_cells[offset + 1].as_str());
+        radar_view[i][5] = RadarCell::from_bits(horizontal_walls_cells[offset + 2].as_str());
         offset += 3;
     }
 
     radar_view
 }
 
-fn fill_radar_view_with_vertical_walls(radar_view: &Vec<Vec<String>>, vertical_walls_cells: &Vec<String>) -> Vec<Vec<String>> {
+fn fill_radar_view_with_vertical_walls(radar_view: &Vec<Vec<RadarCell>>, vertical_walls_cells: &Vec<String>) -> Vec<Vec<RadarCell>> {
     let mut radar_view = radar_view.clone();
     let mut offset = 0;
 
     for i in (0..radar_view.len()).step_by(2) {
-        radar_view[1][i] = vertical_walls_cells[offset].clone();
-        radar_view[3][i] = vertical_walls_cells[offset + 1].clone();
-        radar_view[5][i] = vertical_walls_cells[offset + 2].clone();
+        radar_view[1][i] = RadarCell::from_bits(vertical_walls_cells[offset].as_str());
+        radar_view[3][i] = RadarCell::from_bits(vertical_walls_cells[offset + 1].as_str());
+        radar_view[5][i] = RadarCell::from_bits(vertical_walls_cells[offset + 2].as_str());
         offset += 3;
     }
 
     radar_view
 }
 
-fn fill_radar_view_with_cells(radar_view: &Vec<Vec<String>>, cells: &Vec<String>) -> Vec<Vec<String>> {
+fn fill_radar_view_with_cells(radar_view: &Vec<Vec<RadarCell>>, cells: &Vec<String>) -> Vec<Vec<RadarCell>> {
     let mut radar_view = radar_view.clone();
     let mut offset = 0;
 
     for i in (1..radar_view.len()).step_by(2) {
-        radar_view[1][i] = cells[offset].clone();
-        radar_view[3][i] = cells[offset + 1].clone();
-        radar_view[5][i] = cells[offset + 2].clone();
+        println!("cell line : {:?} {:?} {:?}", 
+        RadarCell::from_bits(cells[offset].as_str()), 
+        RadarCell::from_bits(cells[offset + 1].as_str()),
+         RadarCell::from_bits(cells[offset + 2].as_str()));
+
+         radar_view[i][1] = RadarCell::from_bits(cells[offset].as_str());
+         radar_view[i][3] = RadarCell::from_bits(cells[offset + 1].as_str());
+         radar_view[i][5] = RadarCell::from_bits(cells[offset + 2].as_str());
         offset += 3;
     }
 
